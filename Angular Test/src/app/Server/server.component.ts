750ce71053482a8414478df8ffcc982e0e63350b
Prashanth <prashanthbenny2008@gmail.com>
2017-06-16 20:11:37 +0530

    Angular test update

---
+++ Angular Test/src/app/Server/server.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector : 'app-server',
+    templateUrl : './server.component.html',
+    styles : [`
+        .online{
+            color: white;
+        }
+    `]
+})
+export class ServerComponent{
+    serverId:number = 1;
+    serverStatus:string = 'Online';  
+
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'Online': 'Offline';
+    }
+    
+    getServerStatus(){
+        return this.serverStatus;
+    }  
+
+    getColor(){
+        return this.serverStatus === 'Online' ? 'green' : 'red';
+    }
+}
